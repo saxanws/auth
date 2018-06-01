a946ce06e06413a5d061cc42d5434d20b11100f4
Axan Similien <saxanws@ldsbc.edu>
2018-05-31 19:18:48 -0600

    Couple lectures in section 10

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
-import {TextInput, View} from 'react-native';
+import {Text} from 'react-native';
 import {Button, CardSection, Card, Input}from './common/index';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
-    state = {email:'', password: ''};
+    state = {email:'', password: '', error: ''};
+
+    onButtonPress() {
+        const {email, password} = this.state;
+        this.setState({error:''});
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({error: 'Authentication failed.'});
+                    });
+            });
+    }
     render() {
         console.log(this.state.email);
         return (
@@ -27,12 +40,24 @@ class LoginForm extends Component {
                     />
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
                 <CardSection>
-                    <Button>Login</Button>
+                    <Button onPress = {this.onButtonPress.bind(this)}>Login</Button>
                 </CardSection>
             </Card>
         );
     };
 };
+
+const styles = {
+    errorTextStyle : {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+}
 
 export default LoginForm;
